98ed185907e9b43a92960d063b16b3cd5b6cb558
EgiStr <egicuco50@gmail.com>
2021-01-28 19:32:14 +0700

    create model post

--- reactdjango/settings.py
+++ reactdjango/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-  
 
   'django.contrib.admin',
   'django.contrib.auth',
@@ -39,6 +38,10 @@ INSTALLED_APPS = [
   'django.contrib.sessions',
   'django.contrib.messages',
   'django.contrib.staticfiles',
+  # local app
+  'posts',
+
+  # external app
 ]
 
 MIDDLEWARE = [
@@ -106,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Jakarta'
 
 USE_I18N = True
 
@@ -118,12 +121,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/assets/'
+# STATIC_URL = '/assets/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-  os.path.join(BASE_DIR, 'build'),
+  os.path.join(BASE_DIR, 'build/static'),
 ]
+MEDIA_URL = '/media/'
+
+STATIC_MEDIA = os.path.join(BASE_DIR,'build/static/media') 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
