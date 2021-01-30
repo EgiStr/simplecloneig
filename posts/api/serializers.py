75492747fa0f7dcbb1ecfbdc16b5f1a44e31fe4a
EgiStr <egicuco50@gmail.com>
2021-01-30 18:06:40 +0700

    perdetail listapi

--- posts/api/serializers.py
+++ posts/api/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework import fields
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer,HyperlinkedIdentityField,SerializerMethodField
 
 from posts.models import Post
 
 class PostSerializer(ModelSerializer):
+    detail = HyperlinkedIdentityField(
+        view_name='api-post:detail',
+        lookup_field='pk' ,  
+    )
+    user = SerializerMethodField()
+    likes = SerializerMethodField()
     class Meta:
         model = Post
         fields = [
+            'detail',
             'id',
             'user',
             'caption',
@@ -14,11 +21,24 @@ class PostSerializer(ModelSerializer):
             'likes',
             'create_at',
         ]
+    def get_user(self,obj):
+        return obj.user.nickname
     
+    def get_likes(self,obj):
+        return obj.likes.count()
+
 
 class PostDetailSerialzer(ModelSerializer):
+    user = SerializerMethodField()
+    likes_count = SerializerMethodField()
     class Meta:
         model = Post
         fields = '__all__'
-        read_only_fields =('id','user','post','create_at','width_field','height_field')
+        read_only_fields =('id','user','post','create_at','width_field','height_field','likes_count')
+        
+    def get_user(self,obj):
+        return obj.user.nickname
+    
+    def get_likes_count(self,obj):
+        return obj.likes.count()
 
