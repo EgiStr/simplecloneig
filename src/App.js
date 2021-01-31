494f6cad08471422d4411a09cc03c1588b26029c
muhammad jawahiruzzaman <m.jawahiruzzaman@gmail.com>
2021-01-31 12:54:11 +0700

    CONTENT

--- src/App.js
+++ src/App.js
@@ -7,14 +7,12 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <div className="row">
-        <div className="col s9">
-          <Content username="m.jawa_" captions="i don't understand" />
-          <Content username="EgiStr" captions="yes i'm sorry" />
-        </div>
+      <div className="container">
+        <Content username="m.jawa_" captions="i don't understand" imageUrl="https://lh3.googleusercontent.com/2Fz6Fn5zq_hh75oNLsyNqyGSHzPopHojN77Eu6GImw_3bb4JteONR_K8lnCY2nRbZQV9RD7ACVYvTHEEoW6oGt2GNkAVXzsGdHl1XI9JWwr9ojo3N7t5mYgqaux8lESdvi4mJTti4Ok=w400" />
+        <Content username="EgiStr" captions="yes i'm sorry" imageUrl="https://lh3.googleusercontent.com/2Fz6Fn5zq_hh75oNLsyNqyGSHzPopHojN77Eu6GImw_3bb4JteONR_K8lnCY2nRbZQV9RD7ACVYvTHEEoW6oGt2GNkAVXzsGdHl1XI9JWwr9ojo3N7t5mYgqaux8lESdvi4mJTti4Ok=w400" />
       </div>
-
     </div>
+
 
   )
 }
--- src/components/content.js
+++ src/components/content.js
@@ -4,21 +4,24 @@ import Avatar from "@material-ui/core/Avatar";
 
 function Content({ username, captions, imageUrl }) {
     return (
-
-        <div className="content container"> 
-            <div className="head">
-                <Avatar
-                    className="avatar"
-                    alt="hasyim"
-                    src="https://lh3.googleusercontent.com/2Fz6Fn5zq_hh75oNLsyNqyGSHzPopHojN77Eu6GImw_3bb4JteONR_K8lnCY2nRbZQV9RD7ACVYvTHEEoW6oGt2GNkAVXzsGdHl1XI9JWwr9ojo3N7t5mYgqaux8lESdvi4mJTti4Ok=w400"
-                />
-                <h6>{username}</h6>
+        <div className="row">
+            <div className="col s8">
+                <div className="content">
+                    <div className="head">
+                        <Avatar
+                            className="avatar"
+                            alt="hasyim"
+                            src={imageUrl}
+                        />
+                        <h6>{username}</h6>
+                    </div>
+                    <img className="contentImage" src={imageUrl} />
+                    <h6 className="caption"><b>{username}</b> {captions}</h6>
+                </div>
             </div>
-            <img className="contentImage" src="https://lh3.googleusercontent.com/2Fz6Fn5zq_hh75oNLsyNqyGSHzPopHojN77Eu6GImw_3bb4JteONR_K8lnCY2nRbZQV9RD7ACVYvTHEEoW6oGt2GNkAVXzsGdHl1XI9JWwr9ojo3N7t5mYgqaux8lESdvi4mJTti4Ok=w400" />
-            <h6 className="caption"><b>{username}</b> {captions}</h6>
         </div>
     )
-}   
+}
 
 export default Content
 
