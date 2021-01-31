c609dc9cbf941e1616aa3b9e461d019031b3249e
muhammad jawahiruzzaman <m.jawahiruzzaman@gmail.com>
2021-01-31 21:03:50 +0700

    rehat

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,9 +8,9 @@ function Navbar() {
                     <div class="nav-wrapper">
                         <a href="/" class="brand-logo ">Logo</a>
                         <ul class="right hide-on-med-and-down">
-                            <li><a href="/"><i class="material-icons">search</i></a></li>
-                            <li><a href="/username"><i class="material-icons">view_module</i></a></li>
-                            <li><a href="/message"><i class="material-icons">refresh</i></a></li>
+                            <li><a href="/"><i class="material-icons">home  </i></a></li>
+                            <li><a href="/username"><i class="material-icons">message</i></a></li>
+                            <li><a href="/message"><i class="material-icons">people</i></a></li>
                         </ul>
                     </div>
                 </div>
