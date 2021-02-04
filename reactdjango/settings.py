ba582af7e99be8eae290c905889c4bf2506dd562
EgiStr <egicuco50@gmail.com>
2021-02-04 14:29:33 +0700

    add django cors headers

--- reactdjango/settings.py
+++ reactdjango/settings.py
@@ -45,15 +45,21 @@ INSTALLED_APPS = [
 
   # external app
   'rest_framework',
+  'corsheaders',
 ]
 
 MIDDLEWARE = [
+   'corsheaders.middleware.CorsMiddleware',
   'django.middleware.security.SecurityMiddleware',
   'django.contrib.sessions.middleware.SessionMiddleware',
   'django.middleware.common.CommonMiddleware',
   'django.contrib.auth.middleware.AuthenticationMiddleware',
   'django.contrib.messages.middleware.MessageMiddleware',
   'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",
 ]
 
 
