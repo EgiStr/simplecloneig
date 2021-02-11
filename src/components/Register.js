cf937db6b429f5e4c57101e77f1e15fc7512268f
muhammad jawahiruzzaman <m.jawahiruzzaman@gmail.com>
2021-02-11 17:30:20 +0700

    register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,60 +1,60 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import axios from 'axios'
-import {Redirect} from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 class Register extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state ={
-      username : '',
-      password : '',
-      password2 : 'l',
-      email : '',
-      validate : false,
-      redirect :false,
-      error : 'ada yang salaah',
+    this.state = {
+      username: '',
+      password: '',
+      password2: 'l',
+      email: '',
+      validate: false,
+      redirect: false,
+      error: 'ada yang salaah',
     }
     this.handlePasswordValidate = this.handlePasswordValidate.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  handleUsername(event){
-    this.setState({username:event.target.value})
+  handleUsername(event) {
+    this.setState({ username: event.target.value })
   }
 
-  handleEmail(event){
-    this.setState({email:event.target.value})
+  handleEmail(event) {
+    this.setState({ email: event.target.value })
   }
 
-  handlePassword(event){
-    this.setState({password:event.target.value})
+  handlePassword(event) {
+    this.setState({ password: event.target.value })
   }
-  handlePasswordValidate(event){
-    this.setState({password2:event.target.value,validate:this.state.password === event.target.value})
+  handlePasswordValidate(event) {
+    this.setState({ password2: event.target.value, validate: this.state.password === event.target.value })
   }
-  
+
   handleSubmit(e) {
     e.preventDefault();
     axios({
-      method:'post',
-      url:'http://127.0.0.1:8000/auth/register/',
-      data : {
+      method: 'post',
+      url: 'http://127.0.0.1:8000/auth/register/',
+      data: {
         username: this.state.username,
         password: this.state.password,
-        password2:this.state.password2,
-        email:this.state.email,
+        password2: this.state.password2,
+        email: this.state.email,
       }
     })
-    .then((res) => {
+      .then((res) => {
         console.log(res)
         this.setState({ redirect: true });
       })
       .catch(e => {
-      if(e.request.status === 400){console.clear();} 
-      this.setState({ username : '',password : '',password2 : '1',email : '',validate : true,redirect :false,error:e.request.response })
-    })
+        if (e.request.status === 400) { console.clear(); }
+        this.setState({ username: '', password: '', password2: '1', email: '', validate: true, redirect: false, error: e.request.response })
+      })
   }
-  render(){
-    if(this.state.redirect){
+  render() {
+    if (this.state.redirect) {
       return <Redirect to="/login" />
     }
     // console.log(this.state.username);
@@ -62,54 +62,56 @@ class Register extends Component {
     // console.log(this.state.password);
     // console.log(this.state.password2);
     return (
-      <div>
-        <h1>CloneIG</h1>
-        <div className="input-field">
-          <input id="icon_prefix" type="text" className="validate" onChange={(event) => {
-            this.handleUsername(event);
-          }} />
-          <label htmlFor="icon_prefix">username</label>
+      <div className="container_login">
+        <div className="box_login">
+          <h5>CloneIG</h5>
+          <div className="input-field">
+            <input id="icon_prefix" type="text" className="validate" onChange={(event) => {
+              this.handleUsername(event);
+            }} />
+            <label htmlFor="icon_prefix">username</label>
+          </div>
+          <div className="input-field">
+            <input id="icon_prefix2" type="text" className="validate" onChange={(event) => {
+              this.handleEmail(event);
+            }} />
+            <label htmlFor="icon_prefix2">email</label>
+          </div>
+          <div className="input-field">
+            <input id="icon_prefix3" type="password" className="validate" onChange={(event) => {
+              this.handlePassword(event);
+            }} />
+            <label htmlFor="icon_prefix3">password</label>
+          </div>
+          <div className="input-field">
+            <input id="icon_prefix4" type="password" className="validate" onChange={(event) => {
+              this.handlePasswordValidate(event);
+            }} />
+            <label htmlFor="icon_prefix4">password confirm</label>
+          </div>
+          {this.state.validate ? (<p>{this.state.error}</p>) : (null)}
+          {this.state.validate ?
+            (<button
+              className="btn waves-effect waves-light"
+              type="submit"
+              onClick={this.handleSubmit}>
+              Submit
+              <i className="material-icons right">send</i>
+            </button>) :
+            (<button
+              className="btn waves-effect waves-light disabled"
+              type="submit"
+              onClick={this.handleSubmit}
+            >
+              Submit
+              <i className="material-icons right">send</i>
+            </button>)}
+            have an account? <a href="/login">Log in</a>
         </div>
-        <div className="input-field">
-          <input id="icon_prefix2" type="text" className="validate" onChange={(event) => {
-            this.handleEmail(event);
-          }} />
-          <label htmlFor="icon_prefix2">email</label>
-        </div>
-        <div className="input-field">
-          <input id="icon_prefix3" type="password" className="validate" onChange={(event) => {
-            this.handlePassword(event);
-          }} />
-          <label htmlFor="icon_prefix3">password</label>
-        </div>
-        <div className="input-field">
-          <input id="icon_prefix4" type="password" className="validate" onChange={(event) => {
-            this.handlePasswordValidate(event);
-          }}  />
-          <label htmlFor="icon_prefix4">password confirm</label>
-        </div>
-        { this.state.validate ? (<p>{this.state.error}</p>) : (null)}
-        { this.state.validate  ? 
-        (<button
-          className="btn waves-effect waves-light"
-          type="submit"
-          onClick={this.handleSubmit}>
-          Submit
-          <i className="material-icons right">send</i>
-        </button>) : 
-        (<button
-          className="btn waves-effect waves-light disabled"
-          type="submit"
-          onClick={this.handleSubmit}
-        >
-          Submit
-          <i className="material-icons right">send</i>
-        </button>)}
-        
       </div>
     );
   }
-  
+
 }
 
 export default Register;
--- src/components/login.js
+++ src/components/login.js
@@ -73,50 +73,52 @@ class Login extends Component {
 
     return (
       <div className="container_login">
-        <h5>Sign in</h5>
-        <div className="input-field">
-          <i className="material-icons prefix">person</i>
-          <input
-            type="text"
-            value={this.state.title}
-            onChange={(event) => {
-              this.handleTitleChange(event);
-            }}
-            placeholder="What password ? ...."
+        <div className="box_login">
+          <h5>Sign in</h5>
+          <div className="input-field">
+            <i className="material-icons prefix">person</i>
+            <input
+              type="text"
+              value={this.state.title}
+              onChange={(event) => {
+                this.handleTitleChange(event);
+              }}
+              placeholder="What password ? ...."
 
-          />
-        </div>
-        <div className="input-field">
-          <i className="material-icons prefix">https</i>
-          <input
-            type="password"
-            value={this.state.password}
-            onChange={(event) => {
-              this.handlePasswordChange(event);
-            }}
-            placeholder="What password ? ...."
-            id="icon_prefix"
-          />
-        </div>
-        <button
-          className="btn waves-effect waves-light"
-          type="submit"
-          onClick={this.handleSubmit}
-        >
-          Submit
-          <i className="material-icons right">send</i>
-        </button>
-        <div className="line"><span>OR</span></div>
-        <div className="icon_box">
-          <div className="icon_field">
-            <i className="material-icons">facebook</i>
+            />
           </div>
-          <div className="icon_field">
-            <i className="material-icons">email</i>
-          </div>  
-        </div>
+          <div className="input-field">
+            <i className="material-icons prefix">https</i>
+            <input
+              type="password"
+              value={this.state.password}
+              onChange={(event) => {
+                this.handlePasswordChange(event);
+              }}
+              placeholder="What password ? ...."
+              id="icon_prefix"
+            />
+          </div>
+          <button
+            className="btn waves-effect waves-light"
+            type="submit"
+            onClick={this.handleSubmit}
+          >
+            Submit
+          <i className="material-icons right">send</i>
+          </button>
+          <div className="line"><span>OR</span></div>
+          <div className="icon_box">
+            <div className="icon_field">
+              <i className="material-icons">facebook</i>
+            </div>
+            <div className="icon_field">
+              <i className="material-icons">email</i>
+            </div>
+          </div>
         You dont have account? <a href="/register">sign up</a>
-        {content}
+          {content}
+        </div>
       </div>
     );
   }
